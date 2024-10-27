e8600d44b1756e9629a3090562128d70cc3d9d3d
pankajyadav8523 <py9621008569@gmail.com>
2024-10-27 10:33:37 +0530

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 const fs = require("fs");
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/api", (req, res) => {
   fs.readFile("MOCK_DATA.json", "utf8", (err, data) => {
     if (err) {
@@ -19,28 +21,28 @@ app.get("/api", (req, res) => {
   });
 });
 
-app.get("/", (req, res) => {
-  fs.readFile("MOCK_DATA.json", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send("Error reading the file.");
-    }
-    try {
-      const jsonData = JSON.parse(data);
+// app.get("/", (req, res) => {
+//   fs.readFile("MOCK_DATA.json", "utf8", (err, data) => {
+//     if (err) {
+//       console.error(err);
+//       return res.status(500).send("Error reading the file.");
+//     }
+//     try {
+//       const jsonData = JSON.parse(data);
 
-      let htmlContent = "<ul>";
-      jsonData.map((value) => {
-        htmlContent += `<li>${value.first_name}</li>`;
-      });
-      htmlContent += "</ul>";
+//       let htmlContent = "<ul>";
+//       jsonData.map((value) => {
+//         htmlContent += `<li>${value.first_name}</li>`;
+//       });
+//       htmlContent += "</ul>";
 
-      res.send(htmlContent);
-    } catch (parseError) {
-      console.error(parseError);
-      res.status(500).send("Error parsing JSON data.");
-    }
-  });
-});
+//       res.send(htmlContent);
+//     } catch (parseError) {
+//       console.error(parseError);
+//       res.status(500).send("Error parsing JSON data.");
+//     }
+//   });
+// });
 
 app.get("/test", (req, res) => {
   res.send("This is a test page");
@@ -54,4 +56,10 @@ app.get("/test/:id", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}.`);
+});
+
+app.post("/add", (req, res) => {
+  const { a, b } = req.body;
+  console.log(parseInt(a) + parseInt(b));
+  res.send({ result: parseInt(a) + parseInt(b) });
 });
